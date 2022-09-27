d1c33cd80235772376e2f475f0553d5d79ad98b9
niriot-dotcom <72941421+Niriot-dotcom@users.noreply.github.com>
2022-09-27 15:45:26 -0500

    navbar align horizontally.

--- src/components/common/AppNavbar.jsx
+++ src/components/common/AppNavbar.jsx
@@ -1,32 +1,41 @@
 import React from "react";
+import imgLogoAcmw from "../../images/Logo_ACMW.png"
 
 function AppNavbar() {
   return (
-    <div>
-      <ul id="nav-bar">
-        <li>
-          <a href="/home">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/members">Members</a>
-        </li>
-        <li>
-          <a href="/projects">Projects</a>
-        </li>
-        <li>
-          <a href="/events">Events</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-        <li>
-          <a href="/register">Register</a>
-        </li>
-      </ul>
-    </div>
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <img className="fill-current h-8 w-8 mr-2" src={imgLogoAcmw} alt="logo" />
+        <span className="font-semibold text-xl tracking-tight">UP ACM-W Ags</span>
+      </div>
+      {/* <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div> */}
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <a href="/home" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Home
+          </a>
+          <a href="/members" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Members
+          </a>
+          <a href="/projects" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Projects
+          </a>
+          <a href="/events" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Events
+          </a>
+          <a href="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Contact
+          </a>
+        </div>
+        <div>
+          <a href="/register" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Register</a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
