d4ed1be68d0797b5a1771ca545a3646e5433d0e1
niriot-dotcom <72941421+Niriot-dotcom@users.noreply.github.com>
2022-10-11 15:33:22 -0500

    Updates

---
+++ precache-manifest.1b5b053eefa3ed6a3eb89d54104efc95.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "124b9f0f4947ae23f375",
+    "url": "/website-acmw/static/css/main.a79239c7.chunk.css"
+  },
+  {
+    "revision": "124b9f0f4947ae23f375",
+    "url": "/website-acmw/static/js/main.6454d6fe.chunk.js"
+  },
+  {
+    "revision": "b42c1ff331cc19c9edb1",
+    "url": "/website-acmw/static/js/runtime~main.5c4215cd.js"
+  },
+  {
+    "revision": "e018d4c76d2c59b1c461",
+    "url": "/website-acmw/static/js/2.57fe96c1.chunk.js"
+  },
+  {
+    "revision": "28851e175a3301af76f8a4274e04f003",
+    "url": "/website-acmw/static/media/Logo_Panamericana-ACM-W_Color_Horizontal.28851e17.png"
+  },
+  {
+    "revision": "1accdfbd051be13e05f9e8dc0b3ca700",
+    "url": "/website-acmw/static/media/Logo_Panamericana-ACM-W_Color_Simbolo.1accdfbd.png"
+  },
+  {
+    "revision": "fff0e8d05d7886911306c88c6a68f579",
+    "url": "/website-acmw/static/media/project-3d-modeling.fff0e8d0.png"
+  },
+  {
+    "revision": "4883191cd65fdbc832d038136253f562",
+    "url": "/website-acmw/static/media/project-advising.4883191c.png"
+  },
+  {
+    "revision": "68110d8162b29e3e71bbb589b570b386",
+    "url": "/website-acmw/static/media/project-cp.68110d81.png"
+  },
+  {
+    "revision": "a4dd3b5f606c925806476239d8ca0981",
+    "url": "/website-acmw/static/media/team-events.a4dd3b5f.png"
+  },
+  {
+    "revision": "26bc65225c979efd353c8ea97b0cbbfd",
+    "url": "/website-acmw/static/media/team-social-media.26bc6522.png"
+  },
+  {
+    "revision": "ab8cc8f4337bc6c524026db21499d22e",
+    "url": "/website-acmw/static/media/team-website.ab8cc8f4.png"
+  },
+  {
+    "revision": "94b6c0d157a6a2bf4367d0cf4d831015",
+    "url": "/website-acmw/static/media/project-3d-modeling.94b6c0d1.png"
+  },
+  {
+    "revision": "94b6c0d157a6a2bf4367d0cf4d831015",
+    "url": "/website-acmw/static/media/project-advising.94b6c0d1.png"
+  },
+  {
+    "revision": "94b6c0d157a6a2bf4367d0cf4d831015",
+    "url": "/website-acmw/static/media/project-cp.94b6c0d1.png"
+  },
+  {
+    "revision": "e9d0b36bee77f66a2b5f6faea893538d",
+    "url": "/website-acmw/index.html"
+  }
+];
--- src/pages/Events.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Events() {
-  return <div>Eventos</div>;
-}
-
-export default Events;
